956d7f98c8549a55b3829ebd82ff7fb135ff4ac5
Mariana Alves <marianasdev@gmail.com>
2025-09-22 22:40:18 +0000

    feat: adicionando meu primeiro código em Java utilizando o método Scanner

---
+++ INTRODUCAO_001/metodo_scanner.java
@@ -0,0 +1,25 @@
+package INTRODUCAO_001;
+import java.util.Scanner;
+
+public class metodo_scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Digite sua cidade: ");
+        String cidade = sc.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int idade = sc.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        float altura = sc.nextFloat();
+
+        System.out.println("\nDados:");
+        System.out.printf("Olá, %s! Você mora em %s, tem %d anos e %.2f de altura.", nome, cidade, idade, altura);
+
+        sc.close();
+    }
+}
