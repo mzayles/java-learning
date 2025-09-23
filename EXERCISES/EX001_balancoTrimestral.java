f75c553ca040a8a2bbdce1654db0f80452da4c17
Mariana Alves <marianasdev@gmail.com>
2025-09-23 03:08:00 +0000

    feat: início dos exercícios

---
+++ EXERCISES/EX001_balancoTrimestral.java
@@ -0,0 +1,17 @@
+package EXERCISES;
+
+public class EX001 {
+    public class static void main(Strings[] args) {
+        float gastosJaneiro;
+        float gastosFevereiro;
+        float gastosMarco;
+        float gastoTotal;
+        
+        gastosJaneiro = 15000f;
+        gastosFevereiro = 23000f;
+        gastosMarco = 17000f;
+
+        gastosTrimestre = (gastosJaneiro + gastosFevereiro + gastosMarco);
+        System.out.printf("Gasto total do trimestre: %.2f", gastosTrimestre)
+    }
+}
